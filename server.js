4a452cd18fadac923d5dda50779cab4e1b309fb5
Kelvin <knunez90@hotmail.com>
2021-01-30 11:25:04 -0500

    Express Routing Workshop

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -11,7 +13,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/campsites', campsiteRouter)
-
+app.use('/promotions', promotionRouter)
+app.use('/partners', partnerRouter )
 
 
 app.use(express.static(__dirname + '/public'))
@@ -19,7 +22,7 @@ app.use(express.static(__dirname + '/public'))
 app.use((req,res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Thsi is an Express Server!</h1></body></html>');
+    res.end('<html><body><h1>This is an Express Server!</h1></body></html>');
 });
 
 app.listen(port, hostname, () => {
